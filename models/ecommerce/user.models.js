3cb317c2cc02c901b4df96d4858d5f69faed0fea
SulemanWaraich <tech4you330@gmail.com>
2024-11-23 17:28:45 +0500

    ecommerce schema

--- models/ecommerce/user.models.js
+++ models/ecommerce/user.models.js
@@ -2,7 +2,22 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
     {
-        
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
     }, 
     {timestamps: true});
 
